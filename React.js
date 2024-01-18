3dc34f78502069f06cb65158cc2665db03b7e412
Maniprabha S <108254371+Maniprabha06@users.noreply.github.com>
2024-01-18 22:20:18 +0530

    Update React.js

--- React.js
+++ React.js
@@ -163,3 +163,51 @@ const Content = () => {
 }
 
 export default Content
+
+List and Keys:
+Content.js
+import React from 'react'
+import { useState } from 'react';
+
+const Content = () => {
+    const [items, setItems] = useState(
+      [
+        {id:1,
+        checked : true,
+        item : "Practice coding"
+      },
+      {id:1,
+        checked : true,
+        item : "Learn React"
+      },
+      {id:1,
+        checked : true,
+        item : "Play games"
+      }
+
+      ]);
+      // const numbers = [-2,0,3,4,5];
+      // const itemss = numbers.map(n=> ({number : n}))
+      // const itemss= numbers.filter(n=> n>=0).map(n=>({number :n}))
+      // console.log(itemss)
+  return (
+    <main>
+      <ul>
+        <li>
+          {items.map((item) => (
+            <li>
+              <input 
+              type=  "checkbox"
+              checked= {item.checked}
+              />
+              <label>{item.item}</label>
+              <button>Delete</button>
+            </li>
+          ))}
+        </li>
+      </ul>
+    </main>
+  )
+}
+
+export default Content
